05e271290b40b8e883b441390b170f1368c94c01
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-08-17 10:16:44 -0700

    Setup schema

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`);
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+const wordSchema = new Schema({
+  id: Number,
+  word: String,
+  definition: String,
+})
+
+
+const Word = mongoose.model('Word', wordSchema);
+
+let save = (word, definition) => {
+}
+
+module.exports.Word = Word;
+module.exports.save = save;
