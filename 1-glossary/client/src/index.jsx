5b917563af524b91793a241e99cb720864b9de6c
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-08-17 13:36:23 -0700

    Added form with handleSubmit and handleChange

--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -10,30 +10,51 @@ class App extends React.Component {
     this.state = {
       wordCount: 0,
       wordList: [],
+      wordInput: '',
+      definitionInput: '',
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
   }
 
   componentDidMount() {
-
     axios.get('/glossary')
       .then((response) => {
-        console.log('data here: ', response.data.length);
-
         this.setState({
           wordCount: response.data.length,
           wordList: response.data,
         })
-
       })
+  }
 
+  handleChange(e){
+    this.setState({
+      [e.target.id]: e.target.value,
+    })
+  }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    alert('something was submitted???');
   }
 
   render() {
     return (
       <React.Fragment>
-        <h1>Word Glossary App</h1>
+        <h1>Glossary App</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Word:
+            <input type="text" value={this.state.wordInput} onChange={this.handleChange} id="wordInput"/>
+          </label>
+          <label>
+            definition:
+            <input type="text" value={this.state.definitionInput} onChange={this.handleChange} id="definitionInput"/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <ul>Words in db: {this.state.wordCount}</ul>
         <WordList words={this.state.wordList} />
       </React.Fragment>
